f57723423fc8212a42597dfd161119e99abaab41
linda-lai <lindalai.615@gmail.com>
2020-05-14 01:59:26 +1000

    Implement GET request for all tickets with basic authentication

--- internal/main.go
+++ internal/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"encoding/base64"
 	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
 
 	"github.com/joho/godotenv"
@@ -13,6 +15,26 @@ func basicAuth(username, password string) string {
 	return base64.StdEncoding.EncodeToString([]byte(username + ":" + password))
 }
 
+func getTickets(username, password, subdomain string) string {
+	client := &http.Client{}
+	url := fmt.Sprintf("https://%s.zendesk.com/api/v2/tickets.json", subdomain)
+	req, err := http.NewRequest("GET", url, nil)
+
+	req.Header.Add("Authorization", "Basic "+basicAuth(username, password))
+	// req.SetBasicAuth(username, password)
+
+	resp, err := client.Do(req)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	tickets := string(body)
+
+	return tickets
+}
+
 func main() {
 	err := godotenv.Load()
 	if err != nil {
@@ -20,5 +42,7 @@ func main() {
 	}
 	username := os.Getenv("ZD_USERNAME")
 	password := os.Getenv("ZD_PASSWORD")
+	subdomain := os.Getenv("ZD_SUBDOMAIN")
 	fmt.Println(basicAuth(username, password))
+	fmt.Println(getTickets(username, password, subdomain))
 }
